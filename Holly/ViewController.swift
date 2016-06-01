c622c4d9ccf457300b2f8c81f21aacad0fb17759
Dima Tyurin <tyurin94@gmail.com>
2016-06-01 03:30:46 +0300

    v0.0.1

--- Holly/ViewController.swift
+++ Holly/ViewController.swift
@@ -7,11 +7,28 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player = AVPlayer()
+
+    func configureView() {
+        let url = "http://happyinside.ru/static/assets/img/AveMaria.mp3"
+        let playerItem = AVPlayerItem( URL:NSURL( string:url )! )
+        player = AVPlayer(playerItem:playerItem)
+        player.rate = 1.0;
+        player.play()
+        
+    }
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        configureView()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("viewDidLoad()")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
